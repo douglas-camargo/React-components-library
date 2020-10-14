4f03af6ebe6e49fb4f288892c3b337f193c52c5b
douglas camargo <douglascamargo159@gmail.com>
2020-10-14 17:31:04 -0430

    arreglando detalles

--- src/componentes/Card/Card.jsx
+++ src/componentes/Card/Card.jsx
@@ -22,7 +22,8 @@ const Card = ({id ,title, precio, formato, medida, cantidad, rutaImg, alt, cat, 
       <a class="button dark-color tiny" href="#">Consultar</a>
       </div>
       <div className=" center card__data s-border s-radius-br s-radius-bl s-pxy-2">
-<a href={ficha} className="t5 s-mb-2 s-center" download>Ficha</a>
+        <img src="./images/iconoPDF.png" className="tamañoiconopdf img-container s-mr-1"/>
+        <a href={ficha} className="t5 s-mb-2 s-center colorficha" download>Ficha</a>
       </div> 
     <div className="margen s-main-center s-bg-grey s-radius s-pxy-2 l-block">
       <a className="button--ghost-alert button--tiny" href="#">{precio}</a>
